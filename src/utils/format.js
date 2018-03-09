eea1d868b2d0673625b0add0636e9b7c1b5500f1
Tony Coop <tony@thewholeworldwindow.co.uk>
2018-03-09 16:33:27 +0000

    Adds perPage pagination info.

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,7 +1,12 @@
-import omit from 'lodash.omit'
+import omit from 'lodash/omit'
+import _get from 'lodash/get'
+import qs from 'qs'
+
+console.log(_get)
 
 export default (type, data, req, res) => {
-  let response = { status: 'ok', code: 200, meta: {} }
+  let response = { status: 'ok', code: 200 }
+  const query = qs.parse(req.getQuery())
 
   const paginated = res.paginate.getPaginatedResponse(data)
   const parsed = JSON.parse(JSON.stringify(paginated))
@@ -16,7 +21,8 @@ export default (type, data, req, res) => {
       return acc.concat(newItem)
     }, [])
     response.pages = parsed.pages
-    response.meta.total = data.length
+    response.pages.total = data.length
+    response.pages.perPage = _get(query, 'per_page')
   } else {
     response = null
   }
